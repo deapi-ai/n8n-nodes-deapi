9e313682babf44837b1d519fd61deb10c83dba82
Alex Glowacki <22919693+AleGlowa@users.noreply.github.com>
2026-01-26 13:36:39 +0100

    endpoints router

---
+++ nodes/Deapi/actions/node.type.ts
@@ -0,0 +1,7 @@
+import { AllEntities } from "n8n-workflow";
+
+type NodeMap = {
+  image: 'generate';
+};
+
+export type DeApiType = AllEntities<NodeMap>;
---
+++ nodes/Deapi/actions/router.ts
@@ -0,0 +1,58 @@
+import {
+  IExecuteFunctions,
+  INodeExecutionData,
+  NodeOperationError
+} from 'n8n-workflow';
+
+import type { DeApiType } from './node.type';
+import * as image from './image';
+
+export async function router(this: IExecuteFunctions) {
+  const returnData: INodeExecutionData[] = [];
+
+  const items = this.getInputData();
+  const resource = this.getNodeParameter('resource', 0);
+  const operation = this.getNodeParameter('operation', 0);
+
+  const deApiTypeData = {
+    resource,
+    operation,
+  } as DeApiType;
+
+  let execute;
+  switch (deApiTypeData.resource) {
+    case 'image':
+      execute = image[deApiTypeData.operation].execute;
+      break;
+    default:
+      throw new NodeOperationError(
+        this.getNode(),
+        `The operation "${operation}" is not supported!`
+      );
+  }
+
+  for (let i = 0; i < items.length; i++) {
+    try {
+      const responseData = await execute.call(this, i);
+      returnData.push(...responseData);
+    } catch (error) {
+      if (this.continueOnFail()) {
+        returnData.push({ json: { error: error.message }, pairedItem: { item: i } });
+        continue;
+      } else {
+        // Adding `itemIndex` allows other workflows to handle this error
+        if (error.context) {
+          error.context.itemIndex = i;
+          throw error;
+        }
+
+        throw new NodeOperationError(this.getNode(), error, {
+          itemIndex: i,
+          description: error.description,
+        });
+      }
+    }
+  }
+
+  return [returnData];
+}
