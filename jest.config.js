7aa3aec735c9f95934f5d76ad62a6f09504fa9f6
Alex Glowacki <22919693+AleGlowa@users.noreply.github.com>
2026-02-19 18:08:00 +0100

    - update author, homepage - default devDependencies and scripts from n8n-nodes-starter - default compilerOptions from n8n-nodes-starter, remove unnecassary excludes

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
-const { createDefaultPreset } = require("ts-jest");
+const { createDefaultPreset } = require('ts-jest');
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
-  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+	testEnvironment: 'node',
+	transform: {
+		...tsJestTransformCfg,
+	},
+	testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+	modulePathIgnorePatterns: ['<rootDir>/dist/'],
 };
